b80c22f22048a197f60f1a6933c6dc15fb3946b6
João Marin Jr <joaomarinjr@hotmail.com>
2022-03-18 14:13:39 -0300

    Criação do projeto

---
+++ ConsultantSchedule/Data/ConsultantScheduleContext.cs
@@ -0,0 +1,19 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultantSchedule.Models
+{
+    public class ConsultantScheduleContext : DbContext
+    {
+        public ConsultantScheduleContext (DbContextOptions<ConsultantScheduleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ConsultantSchedule.Models.Consultant> Consultant { get; set; }
+    }
+}
---
+++ ConsultantSchedule/Models/Consultant.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsultantSchedule.Models
+{
+    public class Consultant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Consultant(int id, string name, string phoneNumber)
+        {
+            Id = id;
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
